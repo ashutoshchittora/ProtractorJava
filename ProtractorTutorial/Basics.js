90eb75c30bc5fa62d965467ba69436b3c4cda51a
Ashutosh Chittora <AshutoshChittora@gmail.com>
2019-02-19 14:40:39 +0000

    javascript basics and spec1 has asynchrounous handling using then() command.

---
+++ ProtractorTutorial/Basics.js
@@ -0,0 +1,50 @@
+var a = "hello";
+var b =4;
+console.log(" datatype test staring ... ")
+if(a=="hello"){
+	console.log(" hello checked and passed... ");
+}else {
+	console.log(" else block executed... ");
+}
+
+//  for loop : print number from 1 to 100
+for(var i=1;i<100;i++){
+	console.log(i);
+}
+
+// while loop
+var j=1;
+while (j<5){
+console.log(j);	
+j++;
+}
+
+// do while loop
+var k=10;
+do{
+	console.log(k);	
+}while(k<6)
+	
+// use a function
+console.log("add function ... ");
+console.log(add(400,150));
+function add(a,b){
+	return a+b;
+	
+}
+
+
+// array in javascript
+var b = ["pune","8","mumbai","nagpur","4"];
+for(var k=0;k<b.length;k++){
+	console.log(b[k]);
+}
+
+var c = new Array();
+
+for(var t=0;t<5;t++){
+	c[t] = "String"+t;
+}
+for(var m=0;m<c.length;m++){
+	console.log(c[m]);
+}
--- ProtractorTutorial/spec1.js
+++ ProtractorTutorial/spec1.js
@@ -3,15 +3,21 @@ describe('Protractor Suite', function(){
 	
 	it('Protractor test case1' , function(){
 		browser.get("https://angularjs.org");
-		
+		browser.get("http://juliemr.github.io/protractor-demo/");
+		browser.get("https://angularjs.org").then(function(){
+			console.log("This is a asynchronous block but we have handled this using THEN method ");
+		})
 	})
 	
 	it('Protractor test case2' , function(){
 		
-		
-	})
+		console.log("This is test case2 and to be shown before asynchronous print statement");		
+	})	
+	
 	
 })
+
+console.log("This is a asynchronous block to be execute at the end ... ");
 
 // describe ('suiteName' , function with testcase/(it)blocks) - describe accepts 2 args only
 //it('testCaName' , ) . Each it() block is a tree in jasmine framework
