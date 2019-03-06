f789dfb5b27c109a5dda185df37468bd7ca683c6
Ashutosh Chittora <AshutoshChittora@gmail.com>
2019-03-06 12:02:26 +0000

    typescript files added

---
+++ ProtractorTutorial/TypeScriptFiles/TypeScriptClassDemo.ts
@@ -0,0 +1,38 @@
+export class TypeScriptClassDemo
+{
+  // class constitutes of -- properties , methods and constructors
+  // properties -- variables declared inside a class is treated as properties. It should be accesible to any object outside this class
+  // let vs property : both are same except that property dont use a let keyword and is declared inside a class
+  // 'export' -- use this if you want the memebers of this class to be available to all other objects outside this class.
+  // 'property' in TS = 'prototype' in JS
+
+  username:string;
+  password:string;
+  Ssn:number;
+    
+  /*constructor(){
+    console.log("i am the constructor , i am called at class load and take the first preference ...");
+  }*/
+
+    constructor(ssnReceived:number){
+        this.Ssn = ssnReceived;
+        }
+        
+  getUserName(){
+        return this.username;
+    }
+
+  setUserName(userNameReceived:string){
+        this.username = userNameReceived;
+    }
+
+    getSsn(){
+        return this.Ssn;
+    }
+}
+
+//let typeClass = new TypeScriptClassDemo();
+let typeClass = new TypeScriptClassDemo(123123);
+typeClass.setUserName("hello i am coming  from TypeScriptClassDemo class");
+console.log(typeClass.getUserName());
+console.log(typeClass.getSsn());
